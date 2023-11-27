597128f76eb8c1e779633d3f10dc07556b351cd5
Hamit Mizrak <hamitmizrak@gmail.com>
2023-11-27 16:06:17 +0300

    // null // new // instance // zero // typeof // Cast

--- _4_JavaScript/template.js
+++ _4_JavaScript/template.js
@@ -51,9 +51,40 @@ var number;
 
 /////////////////////////////////////////////////////
 // Prompt
-const uname=prompt("Adınız nedir?");
-document.writeln(uname+"<br/>");
+// const uname=prompt("Adınız nedir?");
+// document.writeln(uname+"<br/>");
 
-// Prompt
-const usurname=prompt("Soyadınız nedir?");
-document.writeln(usurname+"<br/>");
+// // Prompt
+// const usurname=prompt("Soyadınız nedir?");
+// document.writeln(usurname+"<br/>");
+
+/////////////////////////////////////////////////////
+// Cast
+let number1=10;
+let number2=String(Number("20")) ;// cast çevirdi
+document.writeln(number1+number2+"<br/>");
+
+/////////////////////////////////////////////////////
+// typeof
+let number3=20;
+document.writeln(typeof number3+" <br/>");
+let number4="Merhabalar";
+document.writeln(typeof number4+" <br/>");
+let number5=true;
+document.writeln(typeof number5+" <br/>");
+
+/////////////////////////////////////////////////////
+// null
+// new
+// instance
+// zero
+
+// undefined
+let data;
+document.writeln(data+" <br/>");
+
+// Infinity
+let data2=4/0;
+document.writeln(data2+" <br/>");
+
+/////////////////////////////////////////////////////
