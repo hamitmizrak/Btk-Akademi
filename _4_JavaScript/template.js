1468608da4c1f183973dabff0e1a67e346c1cbbc
Hamit Mizrak <hamitmizrak@gmail.com>
2023-11-27 17:18:35 +0300

    Math, String

--- _4_JavaScript/template.js
+++ _4_JavaScript/template.js
@@ -5,86 +5,146 @@ multiple Comment
 */
 
 
-console.log("console.log");
-console.info("console.info");
-console.warn("console.warn");
-console.error("console.error");
+// console.log("console.log");
+// console.info("console.info");
+// console.warn("console.warn");
+// console.error("console.error");
 
-document.writeln("Ekranda göster<br/>");
-window.document.writeln("Ekranda göster-2");
+// document.writeln("Ekranda göster<br/>");
+// window.document.writeln("Ekranda göster-2");
 
-// alert("Pop-up data");
+// // alert("Pop-up data");
 
-/////////////////////////////////////////////////////
-// Variable Global 
+// /////////////////////////////////////////////////////
+// // Variable Global 
 
-var pencil="Digital Pencil";
-var pencil="Digital Pencil-1";
-document.writeln(pencil+"<br/>");
+// var pencil="Digital Pencil";
+// var pencil="Digital Pencil-1";
+// document.writeln(pencil+"<br/>");
 
-// Variable local 
-let pencil2="Digital Pencil-2";
-pencil2="Digital Pencil-22";
-document.writeln(pencil2+"<br/>");
+// // Variable local 
+// let pencil2="Digital Pencil-2";
+// pencil2="Digital Pencil-22";
+// document.writeln(pencil2+"<br/>");
 
-const pencil3="Digital Pencil-3";
-// pencil3="Digital Pencil-22"; yazamazsınız.
-document.writeln(pencil3+"<br/>");
+// const pencil3="Digital Pencil-3";
+// // pencil3="Digital Pencil-22"; yazamazsınız.
+// document.writeln(pencil3+"<br/>");
 
-/////////////////////////////////////////////////////
-// Operatörler
-// + - * / 
-// &&=AND(VE) ||=OR(VEYA)
-// ! DEĞİL
+// /////////////////////////////////////////////////////
+// // Operatörler
+// // + - * / 
+// // &&=AND(VE) ||=OR(VEYA)
+// // ! DEĞİL
 
-// Inceredement
-// DeInceredement
-// x++
-// ++x
-// x--
-// --x
+// // Inceredement
+// // DeInceredement
+// // x++
+// // ++x
+// // x--
+// // --x
 
-/////////////////////////////////////////////////////
-// Hoisting
-number=44;
-var number;
+// /////////////////////////////////////////////////////
+// // Hoisting
+// number=44;
+// var number;
 
-/////////////////////////////////////////////////////
-// Prompt
-// const uname=prompt("Adınız nedir?");
-// document.writeln(uname+"<br/>");
-
+// /////////////////////////////////////////////////////
 // // Prompt
-// const usurname=prompt("Soyadınız nedir?");
-// document.writeln(usurname+"<br/>");
+// // const uname=prompt("Adınız nedir?");
+// // document.writeln(uname+"<br/>");
+
+// // // Prompt
+// // const usurname=prompt("Soyadınız nedir?");
+// // document.writeln(usurname+"<br/>");
+
+// /////////////////////////////////////////////////////
+// // Cast
+// let number1=10;
+// let number2=String(Number("20")) ;// cast çevirdi
+// document.writeln(number1+number2+"<br/>");
+
+// /////////////////////////////////////////////////////
+// // typeof
+// let number3=20;
+// document.writeln(typeof number3+" <br/>");
+// let number4="Merhabalar";
+// document.writeln(typeof number4+" <br/>");
+// let number5=true;
+// document.writeln(typeof number5+" <br/>");
+
+// /////////////////////////////////////////////////////
+// // null
+// // new
+// // instance
+// // zero
+
+// // undefined
+// let data;
+// document.writeln(data+" <br/>");
+
+// // Infinity
+// let data2=4/0;
+// document.writeln(data2+" <br/>");
+
+// NaN
+//  let data3="Merhabalar"/2;
+//  document.writeln(data3+" <br/>");
+
 
 /////////////////////////////////////////////////////
-// Cast
-let number1=10;
-let number2=String(Number("20")) ;// cast çevirdi
-document.writeln(number1+number2+"<br/>");
+// Math
+// document.writeln(Math.PI+" <br/>");
+// document.writeln(Math.E+" <br/>");
+// document.writeln(Math.min(4,5,6,8,10)+" <br/>");
+// document.writeln(Math.max(4,5,6,8,10)+" <br/>");
+// document.writeln(Math.abs(-25)+" <br/>");
+// document.writeln(Math.pow(2,5)+" <br/>");
+// document.writeln(Math.sqrt(25)+" <br/>");
+// document.writeln(Math.floor(4.9)+" <br/>");
+// document.writeln(Math.ceil(4.1)+" <br/>");
+// document.writeln(Math.round(2.4)+" <br/>");
+// document.writeln(Math.round(2.5)+" <br/>");
+// document.writeln(Math.sin(2.5)+" <br/>");
+// document.writeln(Math.asin(1)+" <br/>");
+// document.writeln(Math.cos(2.5)+" <br/>");
+// document.writeln(Math.floor(Math.random()*9+1)+"<br/>");
 
 /////////////////////////////////////////////////////
-// typeof
-let number3=20;
-document.writeln(typeof number3+" <br/>");
-let number4="Merhabalar";
-document.writeln(typeof number4+" <br/>");
-let number5=true;
-document.writeln(typeof number5+" <br/>");
+// String 
+let vocabulary="js ÖĞREniyorum js ";
+document.writeln(vocabulary+" <br/>")
+document.writeln(vocabulary.length+" <br/>")
+document.writeln(vocabulary.trim().length+" <br/>")
+
+document.writeln(vocabulary.toUpperCase()+" <br/>")
+document.writeln(vocabulary.toLowerCase()+" <br/>")
+
+document.writeln(vocabulary.indexOf("js")+" <br/>")
+document.writeln(vocabulary.lastIndexOf("js")+" <br/>");
+document.writeln(vocabulary.charAt(1)+" <br/>");
+
+document.writeln(vocabulary.concat("INC")+" <br/>");
+document.writeln(vocabulary.replace(vocabulary," değiştirdim")+" <br/>");
+
+document.writeln(vocabulary.substring(1)+" <br/>");
+document.writeln(vocabulary.substring(1,5)+" <br/>"); // 1<=X<=5-1
 
 /////////////////////////////////////////////////////
-// null
-// new
-// instance
-// zero
 
-// undefined
-let data;
-document.writeln(data+" <br/>");
+// ÖDEV // kullanıcı tarafından girilen bir kelimenin (prompt)
+// let userData= prompt("Lütfen bir şeyler yazınız")  ;
+// S-1) Kaç karakterlidir ? (length)
+// S-2) boşluklar alınarak Kaç karakterlidir ? (trim)
+// S-3) bütün kelimeyi küçük harfle göstermek ? (toLowerCase())
+// S-4) bütün kelimeyi büyük harfle göstermek ? (toUpperCase())
+// S-5) Kullanıcıdan alınan ilk kelimeyle , "javascript" kelimeyle değiştirin. (replace, substring, indexOf)
+// console.log(userData.replace("xxxxxxx"),"javascript");
+// console.log(userData.replace(userData.substring(0,userData.indexOf(" ")),"javascript"));
+// S-6) yazdığınız kelimede "javascript ile mi başlıyor" ? (startsWith()) 
+// S-7) yazdığınız kelimede "javascript ile mi bitiyor" ? (endsWith())
+// S-8) ilk harf nedir  ? (charAt())
+// S-9) girdiğiniz kelimenin sonuna "-ben js öğreniyorum" (concat())
+// S-10) girdiğiniz 0 ile 4 arasındaki indexi ekranda gösterin? (subString())
 
-// Infinity
-let data2=4/0;
-document.writeln(data2+" <br/>");
 
-/////////////////////////////////////////////////////
