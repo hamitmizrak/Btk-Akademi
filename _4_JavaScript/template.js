d191243b2c5b8cd1c1f385b824a617455807fc44
Hamit Mizrak <hamitmizrak@gmail.com>
2023-11-27 15:42:44 +0300

    Hositing, prompt, operators, variable

--- _4_JavaScript/template.js
+++ _4_JavaScript/template.js
@@ -1,1 +1,59 @@
-// alert("Çalıştı")
+// single comment
+
+/*
+multiple Comment
+*/
+
+
+console.log("console.log");
+console.info("console.info");
+console.warn("console.warn");
+console.error("console.error");
+
+document.writeln("Ekranda göster<br/>");
+window.document.writeln("Ekranda göster-2");
+
+// alert("Pop-up data");
+
+/////////////////////////////////////////////////////
+// Variable Global 
+
+var pencil="Digital Pencil";
+var pencil="Digital Pencil-1";
+document.writeln(pencil+"<br/>");
+
+// Variable local 
+let pencil2="Digital Pencil-2";
+pencil2="Digital Pencil-22";
+document.writeln(pencil2+"<br/>");
+
+const pencil3="Digital Pencil-3";
+// pencil3="Digital Pencil-22"; yazamazsınız.
+document.writeln(pencil3+"<br/>");
+
+/////////////////////////////////////////////////////
+// Operatörler
+// + - * / 
+// &&=AND(VE) ||=OR(VEYA)
+// ! DEĞİL
+
+// Inceredement
+// DeInceredement
+// x++
+// ++x
+// x--
+// --x
+
+/////////////////////////////////////////////////////
+// Hoisting
+number=44;
+var number;
+
+/////////////////////////////////////////////////////
+// Prompt
+const uname=prompt("Adınız nedir?");
+document.writeln(uname+"<br/>");
+
+// Prompt
+const usurname=prompt("Soyadınız nedir?");
+document.writeln(usurname+"<br/>");
