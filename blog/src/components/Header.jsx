7f31043e01620ea1a7910212c448efc299814614
54784286228 <hamitmizrak@gmail.com>
2023-12-11 18:27:11 +0300

    backtotop

--- blog/src/components/Header.jsx
+++ blog/src/components/Header.jsx
@@ -211,7 +211,7 @@ class Header extends Component {
                                     </a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#aboutme">
+                                    <a className="nav-link" href="#about_id">
                                         {this.props.t('about')}
                                     </a>
                                 </li>
