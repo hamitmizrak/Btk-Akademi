07176ef52ba4b08bb4bc0264ff9d3d0ea566409a
54784286228 <hamitmizrak@gmail.com>
2023-12-11 18:52:41 +0300

    Contact

--- blog/src/components/Header.jsx
+++ blog/src/components/Header.jsx
@@ -216,11 +216,11 @@ class Header extends Component {
                                     </a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                                    <a className="nav-link" href="#contact_id">
                                         {this.props.t('contact')}
                                     </a>
                                 </li>
-                                <li className="nav-item dropdown">
+                                {/* <li className="nav-item dropdown">
                                     <a
                                         className="nav-link dropdown-toggle"
                                         href="#"
@@ -231,13 +231,13 @@ class Header extends Component {
                                     >
                                         {t('languages')}
                                     </a>
-                                    <div className="dropdown-menu" aria-labelledby="dropdownId">
+                                     <div className="dropdown-menu" aria-labelledby="dropdownId">
                                         <OtherLanguageReusability />
-                                        {/* <a className="dropdown-item" href="#">
+                                         <a className="dropdown-item" href="#">
                                             Action 1
-                                        </a> */}
-                                    </div>
-                                </li>
+                                        </a> 
+                                    </div> 
+                                </li> */}
                             </ul>
 
                             {/* Dark Mode */}
--- blog/src/components/Main.jsx
+++ blog/src/components/Main.jsx
@@ -230,10 +230,106 @@ class Main extends Component {
                         </div>{/* end about services */}
                     </section>  {/* end about services */}
 
+
+                    {/* start contact_id */}
+                    <section id="contact_id">
+                        <div className="contact_service">
+                            <h2 className='text-center  mb-4 text-dark text-uppercase'> İletişim</h2>
+                            <div className="container">
+                                <div className="row">
+                                    {/* MAP */}
+                                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xxl-6">
+                                        <div className="mapouter">
+                                            <div className="gmap_canvas">
+                                                <iframe
+                                                    className="gmap_iframe"
+                                                    frameBorder={0}
+                                                    scrolling="no"
+                                                    marginHeight={0}
+                                                    marginWidth={0}
+                                                    src="https://maps.google.com/maps?width=600&height=400&hl=en&q=malatya&t=&z=14&ie=UTF8&iwloc=B&output=embed"
+                                                />
+                                                <a href="https://embed-googlemap.com">embed google map</a>
+                                            </div>
+                                            <style
+                                                dangerouslySetInnerHTML={{
+                                                    __html:
+                                                        ".mapouter{position:relative;text-align:right;width:600px;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:600px;height:400px;}.gmap_iframe {width:600px!important;height:400px!important;}"
+                                                }}
+                                            />
+                                        </div>
+
+                                    </div>
+
+                                    {/* FORM */}
+                                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xxl-6">
+                                        <form action="#!" method="post" autoComplete='on'>
+                                            {/* Username */}
+                                            <div className="form-group mt-4 mb-4">
+                                                <label htmlFor="uname">Username</label>
+                                                <input
+                                                    type="text"
+                                                    className="form-control"
+                                                    id="uname"
+                                                    placeholder="kullanıcı adınız"
+                                                    title='kullanıcı adınız'
+                                                />
+                                            </div>
+
+                                            {/* Email */}
+                                            <div className="form-group mb-4">
+                                                <label htmlFor="uemail">Email address</label>
+                                                <input
+                                                    type="email"
+                                                    className="form-control"
+                                                    id="uemail"
+                                                    name="uemail"
+                                                    placeholder="Email adresiniz"
+                                                    title="Email adresiniz"
+                                                />
+                                            </div>
+
+                                            {/* Subject */}
+                                            <div className="form-group mb-4">
+                                                <label htmlFor="usubject">Konu</label>
+                                                <input
+                                                    type="text"
+                                                    className="form-control"
+                                                    id="usubject"
+                                                    name="usubject"
+                                                    placeholder="Konu"
+                                                    title="Email Konusu"
+                                                />
+                                            </div>
+
+                                            {/* Content */}
+                                            <div className="form-group mb-4">
+                                                <label htmlFor="exampleFormControlTextarea1">İçerik</label>
+                                                <textarea
+                                                    className="form-control"
+                                                    id="exampleFormControlTextarea1"
+                                                    rows={3}
+                                                    defaultValue={""}
+                                                />
+                                            </div>
+
+                                            <div className="form-group mt-4 mb-4">
+                                                <input type="reset" id="reset" name="reset" value="Temizle" className="btn btn-danger me-3" />
+                                                <button type="submit" id="submit" name="submit" className="btn btn-primary">Gönder </button>
+                                            </div>
+
+                                        </form>
+
+                                    </div>
+                                </div> {/* end row */}
+                            </div>{/* end container */}
+                        </div>{/* end about services */}
+                    </section>  {/* end about services */}
+
                     {/* start backToTop */}
                     <a href="#header" id="backToTop" className="btn btn-outline-primary">
                         <i class="fa-solid fa-arrow-up"></i>
-                        </a>
+                    </a>
 
                 </main>
                 {/* end codes Main  */}
